2314e48ca4c119a90d44ffd14093cf76af9a1441
Induwara <induwara2k@gmail.com>
2024-01-10 01:11:10 +0530

    trying to fix scroll bar issue

--- src/main/java/lk/ijse/chatApp/controller/MainFormController.java
+++ src/main/java/lk/ijse/chatApp/controller/MainFormController.java
@@ -1,5 +1,24 @@
 package lk.ijse.chatApp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
 public class MainFormController {
+
+
+    @FXML
+    private ScrollPane scrollPane;
+
+
+
+    public void initialize(){
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setStyle("-fx-background-color: transparent; -fx-background-insets: 0; -fx-padding: 0;");
+
+    }
+
+
+
 }
 
