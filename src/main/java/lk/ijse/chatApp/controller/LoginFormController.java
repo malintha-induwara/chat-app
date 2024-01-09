bce7d55ec5543cb668dd177964b7d54e99ffaf0d
Induwara <induwara2k@gmail.com>
2024-01-09 20:45:19 +0530

    add file exploror

--- src/main/java/lk/ijse/chatApp/controller/LoginFormController.java
+++ src/main/java/lk/ijse/chatApp/controller/LoginFormController.java
@@ -3,6 +3,15 @@ package lk.ijse.chatApp.controller;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
 
 public class LoginFormController {
 
@@ -10,8 +19,43 @@ public class LoginFormController {
     private MFXTextField txtFieldName;
 
     @FXML
+    private Circle circleImg;
+
+
+    public void initialize(){
+        loadDefaultImage();
+    }
+
+    private void loadDefaultImage() {
+        Image image = new Image("assets/images/user.png");
+        circleImg.setFill(new ImagePattern(image));
+    }
+
+
+    @FXML
     void btnChatOnAction(ActionEvent event) {
 
+    }
+
+    @FXML
+    void circleImgOnAction(MouseEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        configureFileChooser(fileChooser);
+
+        Window window = ((Node) event.getTarget()).getScene().getWindow();
+        File file = fileChooser.showOpenDialog(window);
+
+        if (file != null) {
+            Image selectedImage = new Image(file.toURI().toString());
+            circleImg.setFill(new ImagePattern(selectedImage));
+        }
+    }
+
+    private void configureFileChooser(FileChooser fileChooser) {
+        fileChooser.setTitle("Select Image File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.jpeg")
+        );
     }
 }
 
