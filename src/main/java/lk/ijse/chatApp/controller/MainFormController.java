2afaf8c120123c56d43b77fda4ff0fbb6ab9e957
Induwara <induwara2k@gmail.com>
2024-01-10 17:38:55 +0530

    ui elements agguested

--- src/main/java/lk/ijse/chatApp/controller/MainFormController.java
+++ src/main/java/lk/ijse/chatApp/controller/MainFormController.java
@@ -1,7 +1,19 @@
 package lk.ijse.chatApp.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class MainFormController {
 
@@ -10,11 +22,31 @@ public class MainFormController {
     private ScrollPane scrollPane;
 
 
+    @FXML
+    private TextField txtMassage;
+
+
+
+    @FXML
+    private VBox vBox;
+
+
 
     public void initialize(){
-        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        scrollPane.setStyle("-fx-background-color: transparent; -fx-background-insets: 0; -fx-padding: 0;");
+
+
+    }
+
+
+    @FXML
+    void btnSendOnAction(MouseEvent event) {
+
+
+    }
+
+
+    @FXML
+    void txtFieldOnAction(ActionEvent event) {
 
     }
 
